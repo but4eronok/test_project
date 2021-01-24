c8944860aa44944d5c5f0cac7332333673d2fa20
Igor Silaev <cokturqi55@mail.ru>
2021-01-24 16:51:55 +0300

    I don't believe it

--- MyProject/src/Cat.java
+++ MyProject/src/Cat.java
@@ -6,9 +6,11 @@ public class Cat {
 		Dog dog1 = new Dog();
 		Dog dog2 = new Dog();
 		Dog dog3 = new Dog();
+		Dog dog4 = new Dog();
 		dog1.name = "Max";
 		dog2.name = "Bella";
 		dog3.name = "Jack";
+		dog4.name = "Uki";
 	}
 	public static class Dog {
 		public String name;
